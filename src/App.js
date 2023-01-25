4663e645a09b5e5f4c739e223efd2cff942277d5
9teekill <tim.marsat@gmail.com>
2023-01-25 16:52:51 +0100

    created repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h>Hello SPC</h>
+      <p> On va apprendre le react!</p>
+    </div>
+  );
+}
+
+export default App;
